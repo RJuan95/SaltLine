3ed618e23f21017ab365c0a45a5af286a1ddd8ed
Jurosale <jurosale@ucsc.edu>
2017-10-04 20:35:18 -0700

    Assets and Non library stuff

---
+++ Assets/Scripts/clickPlayers.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickPlayers : MonoBehaviour {
+
+    public GameObject playerID;
+    GameObject var;
+
+	void OnMouseDown()
+    {
+        var = GameObject.FindGameObjectWithTag("Player");
+        playGames numPlayers = var.GetComponent<playGames>();
+
+        if (playerID.tag == "2players") { numPlayers.players = 2; }
+        else if (playerID.tag == "3players") { numPlayers.players = 3; }
+        else if (playerID.tag == "4players") { numPlayers.players = 4; }
+        else { numPlayers.players = 0; }
+
+    }
+}
---
+++ Assets/Scripts/playGames.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playGames : MonoBehaviour
+{
+
+    public Text displayed;
+    public int players = 0;
+    public GameObject intro;
+
+    bool isSetup = false;
+
+    // Use this for initialization
+    public void Start()
+    {
+        intro = GameObject.FindGameObjectWithTag("Intro");
+       
+
+        displayed.text = "Hello, welcome to Saltline!";
+        int winner = 0;
+        int player = 0;
+        /*
+        while (players == 0)
+        {
+            players = Setup();
+        }
+
+        while (winner != 0)
+        {
+            player++;
+            if (player > players) { player = 1; }
+            startRound(player, players);
+        }
+        startRound(player, players);*/
+    }
+
+    int Setup()
+    {
+        /*
+        int numPlayers;
+        string input = "";
+        textDisplay = "Please click the number of players (2-4)";
+        getline(cin, input);
+        stringstream myStream(input);
+        myStream >> numPlayers;
+        if ((numPlayers < 5) && (numPlayers > 1))
+        {
+            cout << "Great, " << numPlayers << " players it is!" << endl;
+            return numPlayers;
+        }
+        else
+        {
+            cout << "Sorry that's not a valid response. Try again." << endl;
+            return 0;
+        }*/
+        return 0;
+    }
+
+    void startRound(int firstPlayer, int totalPlayers)
+    {
+        /*
+        srand(time(NULL));
+        int totalCards = 3 * totalPlayers + 3;
+        int numCards = totalCards - 2;
+        vector<int> candidates;
+        vector<int> field;
+
+        for (int i = 1; i <= totalCards; i++)
+        {
+            candidates.push_back(i);
+        }
+        for (int i = 0; i < numCards; i++)
+        {
+            int numChosen = rand() % (totalCards - i);
+            field.push_back(candidates[numChosen]);
+            candidates.erase(candidates.begin() + numChosen);
+            cout << field[i] << " ";
+        }
+        cout << endl;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (players != 0 && !isSetup)
+        {
+            displayed.text = "Great, " + players + " players today!";
+            isSetup = true;
+            intro.SetActive(false);
+        }
+    }
+}
