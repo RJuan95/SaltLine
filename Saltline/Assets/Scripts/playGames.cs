4fe5c1a432583a79739519afff1350018776da2e
Jurosale <jurosale@ucsc.edu>
2017-10-05 14:47:58 -0700

    working field generator

--- Saltline/Assets/Scripts/playGames.cs
+++ Saltline/Assets/Scripts/playGames.cs
@@ -11,75 +11,20 @@ public class playGames : MonoBehaviour
     public int players = 0;
     public GameObject intro;
 
-    bool isSetup = false;
+    bool isSetup;
+    int winner;
+    int player;
 
     // Use this for initialization
     public void Start()
     {
+
         intro = GameObject.FindGameObjectWithTag("Intro");
-       
-
         displayed.text = "Hello, welcome to Saltline!";
-        int winner = 0;
-        int player = 0;
-        /*
-        while (players == 0)
-        {
-            players = Setup();
-        }
-
-        while (winner != 0)
-        {
-            player++;
-            if (player > players) { player = 1; }
-            startRound(player, players);
-        }
-        startRound(player, players);*/
-    }
-
-    int Setup()
-    {
-        /*
-        int numPlayers;
-        string input = "";
-        textDisplay = "Please click the number of players (2-4)";
-        getline(cin, input);
-        stringstream myStream(input);
-        myStream >> numPlayers;
-        if ((numPlayers < 5) && (numPlayers > 1))
-        {
-            cout << "Great, " << numPlayers << " players it is!" << endl;
-            return numPlayers;
-        }
-        else
-        {
-            cout << "Sorry that's not a valid response. Try again." << endl;
-            return 0;
-        }*/
-        return 0;
-    }
-
-    void startRound(int firstPlayer, int totalPlayers)
-    {
-        /*
-        srand(time(NULL));
-        int totalCards = 3 * totalPlayers + 3;
-        int numCards = totalCards - 2;
-        vector<int> candidates;
-        vector<int> field;
-
-        for (int i = 1; i <= totalCards; i++)
-        {
-            candidates.push_back(i);
-        }
-        for (int i = 0; i < numCards; i++)
-        {
-            int numChosen = rand() % (totalCards - i);
-            field.push_back(candidates[numChosen]);
-            candidates.erase(candidates.begin() + numChosen);
-            cout << field[i] << " ";
-        }
-        cout << endl;*/
+        isSetup = false;
+        winner = 0;
+        player = 0;
+ 
     }
 
     // Update is called once per frame
@@ -91,5 +36,37 @@ public class playGames : MonoBehaviour
             isSetup = true;
             intro.SetActive(false);
         }
+
+        if (players != 0 && winner == 0)
+        {
+            player++;
+            if (player > players) { player = 1; }
+            startRound(player, players);
+            winner = -1;
+        }
+       
+    }
+
+    void startRound(int firstPlayer, int totalPlayers)
+    {
+
+        int totalCards = 3 * totalPlayers + 3;
+        int numCards = totalCards - 2;
+        List<int> candidates = new List<int>();
+        int[] field = new int[numCards];
+        System.Random rand = new System.Random();
+
+        for (int i = 1; i <= totalCards; i++)
+        {
+            candidates.Add(i);
+        }
+
+        for (int j = 0; j < numCards; j++)
+        {
+            int numChosen = candidates[rand.Next(0, candidates.Count)];
+            field[j] = numChosen;
+            candidates.Remove(numChosen);
+        }
+
     }
 }
