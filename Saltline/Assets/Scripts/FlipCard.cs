2b669716c6f5fa35658858d30ae44e1af1e65962
Jurosale <jurosale@ucsc.edu>
2017-10-11 17:45:09 -0700

    working options

--- Saltline/Assets/Scripts/FlipCard.cs
+++ Saltline/Assets/Scripts/FlipCard.cs
@@ -1,75 +1,205 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlipCard : MonoBehaviour {
 
     public int ID;
     public Material mat;
     GameObject var;
+    GameObject Peak;
+    GameObject Pick;
+    GameObject Next;
     playGames roundStuff;
+    FlipCard peakStuff;
+    FlipCard pickStuff;
+    FlipCard nextStuff;
+    public int player = 0;
+    int index;
+    int number;
+    public bool check = false;
+    public bool take = false;
+    public Text p;
 
     private void Start()
     {
         var = GameObject.FindGameObjectWithTag("Player");
+        Peak = GameObject.FindGameObjectWithTag("RevealOne");
+        Pick = GameObject.FindGameObjectWithTag("Pick");
+        Next = GameObject.FindGameObjectWithTag("Regain");
         roundStuff = var.GetComponent<playGames>();
-        mat.mainTexture = roundStuff.none;
+        peakStuff = Peak.GetComponent<FlipCard>();
+        pickStuff = Pick.GetComponent<FlipCard>();
+        nextStuff = Next.GetComponent<FlipCard>();
+        if (ID >= 0) { mat.mainTexture = roundStuff.none; }
+        p.text = "";
     }
+
     void OnMouseDown()
     {
-        if (gameObject.tag == "card" && roundStuff.makeMove) {
-            int number = roundStuff.Field[ID];
-            
-            switch (number)
+        if (gameObject.tag == "card" && peakStuff.check)
+        {
+            number = roundStuff.Field[ID];
+            index = ID;
+            if (1==1)
             {
-                case 1: mat.mainTexture = roundStuff.one;
-                    break;
-                case 2:
-                    mat.mainTexture = roundStuff.two;
-                    break;
-                case 3:
-                    mat.mainTexture = roundStuff.three;
-                    break;
-                case 4:
-                    mat.mainTexture = roundStuff.four;
-                    break;
-                case 5:
-                    mat.mainTexture = roundStuff.five;
-                    break;
-                case 6:
-                    mat.mainTexture = roundStuff.six;
-                    break;
-                case 7:
-                    mat.mainTexture = roundStuff.seven;
-                    break;
-                case 8:
-                    mat.mainTexture = roundStuff.eight;
-                    break;
-                case 9:
-                    mat.mainTexture = roundStuff.nine;
-                    break;
-                case 10:
-                    mat.mainTexture = roundStuff.ten;
-                    break;
-                case 11:
-                    mat.mainTexture = roundStuff.eleven;
-                    break;
-                case 12:
-                    mat.mainTexture = roundStuff.twelve;
-                    break;
-                case 13:
-                    mat.mainTexture = roundStuff.thirteen;
-                    break;
-                case 14:
-                    mat.mainTexture = roundStuff.fourteen;
-                    break;
-                case 15:
-                    mat.mainTexture = roundStuff.fifthteen;
-                    break;
+                switch (number)
+                {
+                    case 1:
+                        mat.mainTexture = roundStuff.one;
+                        break;
+                    case 2:
+                        mat.mainTexture = roundStuff.two;
+                        break;
+                    case 3:
+                        mat.mainTexture = roundStuff.three;
+                        break;
+                    case 4:
+                        mat.mainTexture = roundStuff.four;
+                        break;
+                    case 5:
+                        mat.mainTexture = roundStuff.five;
+                        break;
+                    case 6:
+                        mat.mainTexture = roundStuff.six;
+                        break;
+                    case 7:
+                        mat.mainTexture = roundStuff.seven;
+                        break;
+                    case 8:
+                        mat.mainTexture = roundStuff.eight;
+                        break;
+                    case 9:
+                        mat.mainTexture = roundStuff.nine;
+                        break;
+                    case 10:
+                        mat.mainTexture = roundStuff.ten;
+                        break;
+                    case 11:
+                        mat.mainTexture = roundStuff.eleven;
+                        break;
+                    case 12:
+                        mat.mainTexture = roundStuff.twelve;
+                        break;
+                    case 13:
+                        mat.mainTexture = roundStuff.thirteen;
+                        break;
+                    case 14:
+                        mat.mainTexture = roundStuff.fourteen;
+                        break;
+                    case 15:
+                        mat.mainTexture = roundStuff.fifthteen;
+                        break;
 
+                }
             }
+            peakStuff.check = false;
+            nextPlayer();
+            if (player == 1) { roundStuff.r1[index] = true; }
+            if (player == 2) { roundStuff.r2[index] = true; }
+            if (player == 3) { roundStuff.r3[index] = true; }
+            if (player == 4) { roundStuff.r4[index] = true; }
 
         }
 
+        if (gameObject.tag == "card" && pickStuff.take) {
+            if (roundStuff.r[ID] == false)
+            {
+                number = roundStuff.Field[ID];
+                index = ID;
+                switch (nextStuff.player)
+                {
+                    case 1:
+                        roundStuff.P1.z = number;
+                        roundStuff.p1.text = "player 1:\nHP: " + roundStuff.P1.x + "\nRP: " + roundStuff.P1.y + "\nStr: " + roundStuff.P1.z;
+                        break;
+                    case 2:
+                        roundStuff.P2.z = number;
+                        roundStuff.p2.text = "player 2:\nHP: " + roundStuff.P2.x + "\nRP: " + roundStuff.P2.y + "\nStr: " + roundStuff.P2.z;
+                        break;
+                    case 3:
+                        roundStuff.P3.z = number;
+                        roundStuff.p3.text = "player 3:\nHP: " + roundStuff.P3.x + "\nRP: " + roundStuff.P3.y + "\nStr: " + roundStuff.P3.z;
+                        break;
+                    case 4:
+                        roundStuff.P4.z = number;
+                        roundStuff.p4.text = "player 4:\nHP: " + roundStuff.P4.x + "\nRP: " + roundStuff.P4.y + "\nStr: " + roundStuff.P4.z;
+                        break;
+                }
+                pickStuff.take = false;
+                nextPlayer();
+                roundStuff.r[ID] = true;
+            }
+        }
+
+        if (gameObject.tag == "Regain") {
+            switch (nextStuff.player)
+            {
+                case 1:
+                    roundStuff.P1.y++;
+                    roundStuff.p1.text = "player 1:\nHP: " + roundStuff.P1.x + "\nRP: " + roundStuff.P1.y;
+                    break;
+                case 2:
+                    roundStuff.P2.y++;
+                    roundStuff.p2.text = "player 2:\nHP: " + roundStuff.P2.x + "\nRP: " + roundStuff.P2.y;
+                    break;
+                case 3:
+                    roundStuff.P3.y++;
+                    roundStuff.p3.text = "player 3:\nHP: " + roundStuff.P3.x + "\nRP: " + roundStuff.P3.y;
+                    break;
+                case 4:
+                    roundStuff.P4.y++;
+                    roundStuff.p4.text = "player 4:\nHP: " + roundStuff.P4.x + "\nRP: " + roundStuff.P4.y;
+                    break;
+            }
+            nextPlayer();
+        }
+        else if (gameObject.tag == "RevealOne")
+        {
+            switch (nextStuff.player)
+            {
+                case 1:
+                    roundStuff.P1.y = roundStuff.P1.y - 2;
+                    check = true;
+                    roundStuff.p1.text = "player 1:\nHP: " + roundStuff.P1.x + "\nRP: " + roundStuff.P1.y;
+                    break;
+                case 2:
+                    roundStuff.P2.y = roundStuff.P2.y - 2;
+                    check = true;
+                    roundStuff.p2.text = "player 2:\nHP: " + roundStuff.P2.x + "\nRP: " + roundStuff.P2.y;
+                    break;
+                case 3:
+                    roundStuff.P3.y = roundStuff.P3.y - 2;
+                    check = true;
+                    roundStuff.p3.text = "player 3:\nHP: " + roundStuff.P3.x + "\nRP: " + roundStuff.P3.y;
+                    break;
+                case 4:
+                    roundStuff.P4.y = roundStuff.P4.y - 2;
+                    check = true;
+                    roundStuff.p4.text = "player 4:\nHP: " + roundStuff.P4.x + "\nRP: " + roundStuff.P4.y;
+                    break;
+            }
+        }
+
+        else if (gameObject.tag == "Pick") {
+            take = true;
+            
+        }
+
+    }
+    private void nextPlayer()
+    {
+        nextStuff.player++;
+        if (nextStuff.player > roundStuff.players) { nextStuff.player = 1; }
+        if (nextStuff.player == 1 && (roundStuff.P1.x <= 0 || roundStuff.P1.z > 0)) { nextStuff.player = 2; }
+        if (nextStuff.player == 2 && (roundStuff.P2.x <= 0 || roundStuff.P2.z > 0)) { nextStuff.player = 3; }
+        if (nextStuff.player == 3 && (roundStuff.P3.x <= 0 || roundStuff.P3.z > 0)) { nextStuff.player = 4; }
+        if (nextStuff.player == 4 && (roundStuff.P4.x <= 0 || roundStuff.P4.z > 0)) { nextStuff.player = 1; }
+
+        if (nextStuff.player == 1) { p.text = "Player 1, your turn"; }
+        else if (nextStuff.player == 2) { p.text = "Player 2, your turn"; }
+        else if (nextStuff.player == 3) { p.text = "Player 3, your turn"; }
+        else if (nextStuff.player == 4) { p.text = "Player 4, your turn"; }
     }
 }
--- Saltline/Assets/Scripts/playGames.cs
+++ Saltline/Assets/Scripts/playGames.cs
@@ -8,14 +8,25 @@ public class playGames : MonoBehaviour
 {
 
     public Text displayed;
+    public Text p1;
+    public Text p2;
+    public Text p3;
+    public Text p4;
+
     public string Instructions;
-    public int players = 0;
+    public int players;
     public GameObject intro;
+    public GameObject options;
     public GameObject field;
     public GameObject field3;
     public GameObject field4;
     public bool makeMove = false;
     public int[] Field;
+    public List<bool> r;
+    public List<bool> r1;
+    public List<bool> r2;
+    public List<bool> r3;
+    public List<bool> r4;
 
     public Texture one;
     public Texture two;
@@ -37,12 +48,31 @@ public class playGames : MonoBehaviour
     bool isSetup;
     int winner;
     int player;
+    int health;
+    int resource;
+
+    public struct playerInfo
+    {
+        public int x, y, z;
+        public playerInfo(int health, int resource, int strength)
+        {
+            x = health;
+            y = resource;
+            z = strength;
+        }
+    }
+
+    public playerInfo P1;
+    public playerInfo P2;
+    public playerInfo P3;
+    public playerInfo P4;
 
     // Use this for initialization
     public void Start()
     {
 
         intro = GameObject.FindGameObjectWithTag("Intro");
+        options = GameObject.FindGameObjectWithTag("options");
         field = GameObject.FindGameObjectWithTag("Field");
         field3 = GameObject.FindGameObjectWithTag("3field");
         field4 = GameObject.FindGameObjectWithTag("4field");
@@ -51,11 +81,11 @@ public class playGames : MonoBehaviour
                 "(gain 2) let your opponent peak at any 5 face down cards\n(gain 4) reveal your card to your opponent\n (free) chose any card on the " +
                 "field to be your card in battle\n(free) surrender; no defense but opponent does not gain resources\n(free) ready for battle; gain 3 if" +
                 " you win battle\n (payment varies) battle; pay the value of your card to attack\n";
-        displayed.text = "Hello, welcome to Saltline! The game where players\nuse limited resources to deplete their opponent's even more \nlimited health. So how many players this time?";
+        displayed.text = "Hello, welcome to Saltline! The game where players\nuse limited resources to deplete their opponent's even more limited health. So how many players this time?";
         isSetup = false;
         winner = 0;
         player = 0;
- 
+
     }
 
     // Update is called once per frame
@@ -63,32 +93,96 @@ public class playGames : MonoBehaviour
     {
         if (players != 0 && !isSetup)
         {
-            
-            displayed.text = "Great, " + players + " players today!";
+
+            displayed.text = "Great, " + players + " players which means" +
+                " the highest card is " + (3 * players + 3) + ". Please make" +
+                " a move or click Instructions if this is your first time playing.";
             isSetup = true;
             intro.SetActive(false);
-      
-            if (players == 2) {
+
+            if (players == 2)
+            {
+                P1 = new playerInfo(9, 20, 0);
+                r1 = new List<bool>();
+                for (int i = 0; i < 7; i++) { r1.Add(false); }
+                p1.text = "player 1:\nHP: " + P1.x + "\nRP: " + P1.y;
+
+                P2 = new playerInfo(9, 20, 0);
+                r2 = new List<bool>();
+                for (int i = 0; i < 7; i++) { r2.Add(false); }
+                p2.text = "player 2:\nHP: " + P2.x + "\nRP: " + P2.y;
+
                 field3.SetActive(false);
                 field4.SetActive(false);
+                r = new List<bool>();
+                for (int i = 0; i < 7; i++) { r.Add(false); }
             }
-            if (players == 3) { field4.SetActive(false); }
-            field.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
-        }
 
+            if (players == 3)
+            {
+                P1 = new playerInfo(12, 25, 0);
+                r1 = new List<bool>();
+                for (int i = 0; i < 10; i++) { r1.Add(false); }
+                p1.text = "player 1:\nHP: " + P1.x + "\nRP: " + P1.y;
+
+                P2 = new playerInfo(12, 25, 0);
+                r2 = new List<bool>();
+                for (int i = 0; i < 10; i++) { r2.Add(false); }
+                p2.text = "player 2:\nHP: " + P2.x + "\nRP: " + P2.y;
+
+                P3 = new playerInfo(12, 25, 0);
+                r3 = new List<bool>();
+                for (int i = 0; i < 10; i++) { r3.Add(false); }
+                p3.text = "player 3:\nHP: " + P3.x + "\nRP: " + P3.y;
+
+                field4.SetActive(false);
+                r = new List<bool>();
+                for (int i = 0; i < 10; i++) { r.Add(false); }
+            }
+
+            if (players == 4)
+            {
+                P1 = new playerInfo(15, 30, 0);
+                r1 = new List<bool>();
+                for (int i = 0; i < 13; i++) { r1.Add(false); }
+                p1.text = "player 1:\nHP: " + P1.x + "\nRP: " + P1.y;
+
+                P2 = new playerInfo(15, 30, 0);
+                r2 = new List<bool>();
+                for (int i = 0; i < 13; i++) { r2.Add(false); }
+                p2.text = "player 2:\nHP: " + P2.x + "\nRP: " + P2.y;
+
+                P3 = new playerInfo(15, 30, 0);
+                r3 = new List<bool>();
+                for (int i = 0; i < 13; i++) { r3.Add(false); }
+                p3.text = "player 3:\nHP: " + P3.x + "\nRP: " + P3.y;
+
+                P4 = new playerInfo(15, 30, 0);
+                r4 = new List<bool>();
+                for (int i = 0; i < 13; i++) { r4.Add(false); }
+                p4.text = "player 4:\nHP: " + P4.x + "\nRP: " + P4.y;
+
+                r = new List<bool>();
+                for (int i = 0; i < 13; i++) { r.Add(false); }
+
+            }
+            field.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+            options.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+            startRound(players);
+        }
+        /*
         if (players != 0 && winner == 0)
         {
             player++;
             if (player > players) { player = 1; }
-            startRound(player, players);
+            moveNow(player, players);
             winner = -1;
-        }
-       
+        }*/
+
     }
 
-    void startRound(int firstPlayer, int totalPlayers)
+    void startRound(int totalPlayers)
     {
-        //bool ready = false;
         int totalCards = 3 * totalPlayers + 3;
         int numCards = totalCards - 2;
         List<int> candidates = new List<int>();
@@ -107,9 +201,16 @@ public class playGames : MonoBehaviour
             //Debug.Log(Field[j]);
             candidates.Remove(numChosen);
         }
-
-        makeMove = true;
-
-        //while (!ready) { }
     }
 }
+    /*
+    void moveNow(int firstPlayer, int totalPlayers) {
+        
+        switch (firstPlayer)
+        {
+            //case 1: playerList = r1;
+                //break;
+        }
+    }
+
+}*/
